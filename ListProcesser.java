831cefe036058a28eedd7f86f940aa7964847d14
Ivy Chang <ivycwq@icloud.com>
2017-03-18 23:04:32 -0700

    adding method implementations

--- ListProcesser.java
+++ ListProcesser.java
@@ -1,5 +1,24 @@
-public class ListProceser {
-  public static<T> List<T> filter(List<T>, Predicate<T> p) {
-    
+import java.util.List;
+import java.util.ArrayList;
+
+public class ListProcesser {
+  public static<T> List<T> filter(List<T> list, Predicate<T> p) {
+    List<T> result = new ArrayList<>();
+    for (T t : list)
+      if (p.test(t))
+        result.add(t);
+    return result;
   }
+
+  public static<T> void forEach(List<T> list, Consumer<T> c) {
+    for (T t : list)
+      c.accept(t);
+  }
+
+  public static<T, R> List<R> map(List<T> list, Function<T, R> f) {
+    List<R> result = new ArrayList<>();
+    for (T t : list)
+      result.add(f.apply(t));
+    return result;
+  } 
 }
